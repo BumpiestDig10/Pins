279a33fdd212fa8a9a75b150f5d0e4572ad69da5
Rishi <rishibothra10@gmail.com>
2021-09-03 05:40:50 +0530

    8 Digit Pins

---
+++ 8-digit-numbers.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE * ptr;
+
+    ptr = fopen("8DigitPins.txt", "w");
+
+    if(ptr == NULL)
+    {
+        printf("Error!\n");
+        exit(1);
+    }
+
+    for(int i = 0; i <= 9; i++)
+    {
+        fprintf(ptr, "0000000%d\n", i);
+    }
+
+    for(int i = 10; i <= 99; i++)
+    {
+        fprintf(ptr, "000000%d\n", i);
+    }
+
+    for(int i = 100; i <= 999; i++)
+    {
+        fprintf(ptr, "00000%d\n", i);
+    }
+
+    for(int i = 1000; i <= 9999; i++)
+    {
+        fprintf(ptr, "0000%d\n", i);
+    }
+
+    for(int i = 10000; i <= 99999; i++)
+    {
+        fprintf(ptr, "000%d\n", i);
+    }
+
+    for(int i = 100000; i <= 999999; i++)
+    {
+        fprintf(ptr, "00%d\n", i);
+    }
+
+    for(int i = 1000000; i <= 9999999; i++)
+    {
+        fprintf(ptr, "0%d\n", i);
+    }
+
+    for(int i = 10000000; i <= 99999999; i++)
+    {
+        fprintf(ptr, "%d\n", i);
+    }
+
+    fclose(ptr);
+}
